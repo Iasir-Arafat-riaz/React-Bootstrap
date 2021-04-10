e841b90b5f6f58efb1e6a96dd9757722a3df82ea
Iasir-Arafat-Riaz <iasir.arafat.riaz@gmail.com>
2021-04-11 04:56:29 +0600

    headline add

--- src/components/TopHeadline/TopHeadline.js
+++ src/components/TopHeadline/TopHeadline.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const TopHeadline = () => {
+    useEffect(()=>{
+        fetch("https://newsapi.org/v2/top-headlines?country=us&apiKey=a757074d6a164334a170b27bb8a57562")
+    })
     return (
         <div>
             
