c7687e8192cc9364990da36cf21cbf172e55f300
Iasir-Arafat-Riaz <iasir.arafat.riaz@gmail.com>
2021-04-10 23:20:27 +0600

    new one commit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import { Button, Card } from "react-bootstrap"
 import News from './components/News/News';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
       <h1>React bootsTrap</h1>
-      <Button variant="success">Success</Button>
+      <Header></Header>
+      
       <News></News>
       <p>add</p>
     </div>
--- src/components/News/News.js
+++ src/components/News/News.js
@@ -4,18 +4,7 @@ import { Button, Card, Spinner, Navbar, Form, Nav, FormControl } from "react-boo
 const News = () => {
     return (
         <div>
-            <Navbar bg="dark" variant="dark">
-                <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-                <Nav className="mr-auto">
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#features">Features</Nav.Link>
-                    <Nav.Link href="#pricing">Pricing</Nav.Link>
-                </Nav>
-                <Form inline>
-                    <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                    <Button variant="outline-info">Search</Button>
-                </Form>
-            </Navbar>
+            
             <h2>Card</h2>
             <Card style={{ width: '18rem' }}>
                 <Card.Img variant="top" src="holder.js/100px180" />
@@ -28,8 +17,7 @@ const News = () => {
                     <Button variant="primary">Go somewhere</Button>
                 </Card.Body>
             </Card>
-            <Spinner animation="border" variant="primary" />
-
+            
 
 
         </div>
